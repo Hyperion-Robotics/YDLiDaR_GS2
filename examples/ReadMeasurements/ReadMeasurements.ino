c0e80afc6a008470d9f8e463944a62897a6bf3ea
George Papamichail <152964062+gi0rg0sPapamichail@users.noreply.github.com>
2024-12-14 13:59:16 +0200

    Update ReadMeasurements.ino

--- examples/ReadMeasurements/ReadMeasurements.ino
+++ examples/ReadMeasurements/ReadMeasurements.ino
@@ -7,8 +7,9 @@ void setup() {
 
   GS_error status = lidar.initialize(1);//initialise 1 device
 
-  if(status != GS_OK){
-    Serial.println("There was an error while initializing the lidar");
+  while(status != GS_OK){
+    Serial.println("There was an error while initializing the lidar tryiing again");
+    status = lidar.initialize(1);
   }
   //avoid K0 , K1, ... some boards have pre-defined macros named this way
   double d_K0, d_B0, d_K1, d_B1, Bias;
@@ -21,7 +22,7 @@ void loop() {
   while(true){
     iter_Scan scan = lidar.iter_scans();//get multiple measurments
     Serial.println();
-    for(int i=0;i<160; i++){
+    for(int i=0;i<MAX_SCAN; i++){
       if(!scan.valid[i]){//check
         continue;
       }
